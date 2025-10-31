c177e91b4bc43f360208dbb27f146c39918c1579
MeG <112762788+MeG0302@users.noreply.github.com>
2025-10-31 01:04:11 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Attempting a different import path. It's possible the file is named 'App.jsx' (uppercase A)
-import App from './App.jsx'; 
+import App from './app.jsx'; 
 
 // Find the 'root' div in your index.html and render your App component inside it
 ReactDOM.createRoot(document.getElementById('root')).render(
